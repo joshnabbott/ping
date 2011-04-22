d814f0a949b81f38d44d8d435721fb745be93d71
Jay Zeschin <jay.zeschin@factorylabs.com>
2011-04-22 14:45:32 -0600

    Ported CSS theme from F+S

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,11 @@ class ApplicationController < ActionController::Base
   protected
 
   alias_method :current_user, :current_credential
+
+  helper_method :logged_in?
+  
+  def logged_in?
+    !!current_credential
+  end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,34 @@
 module ApplicationHelper
+  def session_bug
+    if logged_in?
+      content_tag(:p, "Signed in as <em>#{current_credential.username}</em> | #{link_to('Sign Out', destroy_credential_session_path)}".html_safe, :class => "user_info")
+    else
+      content_tag(:div, link_to('Sign in', new_credential_session_path), :id => 'user')
+    end
+  end
+
+  def nav_item(name, url, options={})
+    current_section = options[:current_section] || controller.controller_name
+    css_class = current_section == options[:section] ? "current" : "non_current"
+    link_to name, url, :class => "#{css_class} #{options[:class]}"
+  end
+
+  def flash_message_tag(type,message)
+    content_tag(:div, content_tag(:div, message, :class => type + ' flash'.html_safe))
+  end
+
+  def flash_messages
+    messages = ''.html_safe
+    %w{ alert notice success warning error }.each do |type|
+      if flash[type.to_sym] || flash.now[type.to_sym]
+        messages += flash_message_tag(type,flash[type.to_sym] || flash.now[type.to_sym])
+      end                               
+    end
+    messages.blank? ? '' : content_tag(:div, messages.html_safe, :id => 'flash_messages')
+  end
+
+  def my_profile_path
+   edit_person_path(current_credential.person)
+  end
+  
 end
