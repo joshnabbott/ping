0949f6db625a8fd6110fc64c25d5d985416d8935
Jay Zeschin <jay.zeschin@factorylabs.com>
2011-04-25 13:05:55 -0600

    Removed unused autogenerated steps

--- features/step_definitions/person_steps.rb
+++
@@ -1,9 +0,0 @@
-When /^I delete the (\d+)(?:st|nd|rd|th) person$/ do |pos|
-  within("table tr:nth-child(#{pos.to_i+1})") do
-    click_link "Destroy"
-  end
-end
-
-Then /^I should see the following people:$/ do |expected_people_table|
-  expected_people_table.diff!(tableish('table tr', 'td,th'))
-end
