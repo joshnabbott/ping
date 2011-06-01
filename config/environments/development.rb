eb203a4a9fb459663b76474c851cd78578c433ce
Jay Zeschin <jay.zeschin@factorylabs.com>
2011-06-01 16:19:20 -0600

    Log to stdout in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ Factorypeople::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.logger = Logger.new(STDOUT)
 end
 
