9746b2071ffbb457150b0163b5dad653bbaf0a6b
Josh N. Abbott <joshnabbott@gmail.com>
2011-06-17 13:47:01 -0600

    Trying to fix smtp issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,11 @@ Factorypeople::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = {
-    :enable_starttls_auto => false,
-    :host                 => 'ping.factorylabs.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings   = {
+    :address              => "localhost",
+    :port                 => 25,
+    :domain               => "ping.factorylabs.com",
+    :enable_starttls_auto => false
   }
 end
