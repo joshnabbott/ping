e64d82aff2d17ba48dbfff1318c5b0f70c36d905
Josh N. Abbott <joshnabbott@gmail.com>
2011-06-17 13:49:17 -0600

    Hopefully the last attempt at fixing send mail issue.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,10 @@ Factorypeople::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings   = {
-    :address              => "localhost",
-    :port                 => 25,
-    :domain               => "ping.factorylabs.com",
+  config.action_mailer.smtp_settings = {
     :enable_starttls_auto => false
+  }
+  config.action_mailer.default_url_options = {
+    :host => 'ping.factorylabs.com'
   }
 end
